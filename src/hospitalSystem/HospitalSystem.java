e72e2283f5584e1acd7431379a8a0f4b42b0af27
Ziad Ashraf <aziad3083@gmail.com>
2025-12-06 20:52:37 +0200

    finishing 50% of the HospitalSystem class

--- src/hospitalSystem/HospitalSystem.java
+++ src/hospitalSystem/HospitalSystem.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class HospitalSystem {
 	private ArrayList<Doctor> doctors = new ArrayList<Doctor>();
-	private ArrayList<Patients> patients = new ArrayList<Patients>();
+	private ArrayList<Patient> patients = new ArrayList<Patient>();
 	private ArrayList<Appointments> appointments = new ArrayList<Appointments>();
 	
 	
@@ -16,11 +16,68 @@ public class HospitalSystem {
 		
 	}
 	
-	public void addPatient(String name, int ID, String email, String phoneNumber, int medicalHistoryID) {
+	public void editDoctor(int ID, String email, String phoneNumber, DoctorSpecialization specialization, RoomNum roomNum) {
+		Doctor doctor = findDoctor(ID);
+		
+		if (doctor == null)
+			return;
+	
+		doctor.setEmail(email);
+		doctor.setPhoneNumber(phoneNumber);
+		doctor.setSpecialization(specialization);
+		doctor.setRoomNum(roomNum);
+	
+	}
+	
+	public void addPatient(int ID, String name, String email, int phoneNumber, int medicalHistoryID) {
 
-		Patien patient = new Patient(name, ID, email, phoneNumber, medicalHistoryID);
+		Patient patient = new Patient (ID, name, email, phoneNumber, medicalHistoryID);
 			
 		patients.add(patient);
+		
+	}
+	
+	public void editPatient(int patientID, String email, int phoneNumber) {
+		Patient patient = findPatient(patientID);
+		if (patient == null)
+			return;
+		
+		patient.setEmail(email);
+		patient.setPhoneNumber(phoneNumber);
+	}
+	
+	public void addAvailableSlot(int doctorID, String date, String time) {
+		Doctor doctor = findDoctor(doctorID);
+		
+		if (doctor == null)
+			return;
 
-}
+		// ADD time to doctor object according to the format needed
+		
+	}
+	
+	
+	// to be implemented when the Doctor and Appointment Classes are pushed to the repo
+	public void bookAppointment() {}
+	public void cancelAppointment() {}
+	public ArrayList<String> getBookedAppointments(int doctorID) {}
+	public ArrayList<String> getAvailableAppointments(int doctorID) {}
+	
+	private Doctor findDoctor(int doctorID) {
+		for (Doctor doctor : doctors) {
+			if (doctor.getID() == doctorID)
+				return doctor;
+		}
+		System.out.println("!Warning: Doctor of entered ID is not found, double check its ID or add the doctor to the system first.");
+		return null;
+	}
+	
+	private Patient findPatient(int patientID) {
+		for (Patient patient: patients) {
+			if (patient.getID() == patientID)
+				return patient;
+		}
+		System.out.println("!Warning: patient of entered ID is not found, double check its ID or add the patient to the system first.");
+		return null;
+	}
 }
