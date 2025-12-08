3ca750b94a2c921ebca9e00507674e0ecd2ad52b
Hakeem-Taha-06 <hakeem.taha06@eng-st.cu.edu.eg>
2025-12-08 23:00:51 +0200

    ...

--- src/hospitalSystem/Main.java
+++ src/hospitalSystem/Main.java
@@ -4,12 +4,7 @@ public class  Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Doctor doctor = new Doctor(0, null, null, 0, DoctorSpecialization.Cardiologist, RoomNum.A1);
-		Patient patient = new Patient(0, null, null, 0, 0);
-		doctor.display();
-		patient.display();
-
-		System.err.println(doctor);
+		System.out.println("Welcome to Hospital Management System");
 	}
 
 }
